8176a2df128fbf8ce32358934b19df96f7e67f85
ael-4x4 <kounoaoto1029@icloud.com>
2026-05-05 19:10:31 +0900

    値調整2

--- src/main/java/jp/reitou_mugicha/mystical/enchantments/weapon/EnchantmentLifeSteal.java
+++ src/main/java/jp/reitou_mugicha/mystical/enchantments/weapon/EnchantmentLifeSteal.java
@@ -47,7 +47,7 @@ public class EnchantmentLifeSteal extends CustomEnchant
     public void onHit(Player attacker, EntityDamageByEntityEvent event, int level)
     {
         double damage = event.getFinalDamage();
-        double healAmount = damage * (level * 0.5);
+        double healAmount = damage * (level * 0.3);
 
         double newHealth = Math.min(
                 attacker.getHealth() + healAmount,
